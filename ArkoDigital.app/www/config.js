b9ff8d5abafeb675612c82c72655834ffe2df7fe
raquel-nobre <raquelsouzanobre@gmail.com>
2021-11-16 16:00:06 -0300

    first commit

---
+++ ArkoDigital.app/www/config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'arko-ios-v1',
+		wp_ws_url : 'https://arko.digital/wp-appkit-api/arko-ios-v1',
+		wp_url : 'https://arko.digital',
+		theme : 'q-ios',
+		version : '1',
+		app_type : 'cordova-template',
+		app_title : 'Arko',
+		app_platform : 'ios',
+		app_path: '',
+		gmt_offset : -3,
+		debug_mode : 'on',
+		auth_key : 'VW>*kGCvE<@s]Zq~#Ty&jk|;NyP63X2 r>$M,b$k PqLZP%7V7t0.e E@[T$WP``',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
